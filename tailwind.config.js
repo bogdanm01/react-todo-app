ea5c0969c8098260f1b1f49b636de061b56bc95c
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2024-10-05 15:57:17 +0200

    Configure tailwind theme, import font and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      // Light Theme
+      light: {
+        "very-light-gray": "hsl(0, 0%, 98%)",
+        "very-light-grayish-blue": "hsl(236, 33%, 92%)",
+        "light-grayish-blue": "hsl(233, 11%, 84%)",
+        "dark-grayish-blue": "hsl(236, 9%, 61%)",
+        "very-dark-grayish-blue": "hsl(235, 19%, 35%)",
+      },
+      // Dark Theme
+      dark: {
+        "very-dark-blue": "hsl(235, 21%, 11%)",
+        "very-dark-desaturated-blue": "hsl(235, 24%, 19%)",
+        "light-grayish-blue": "hsl(234, 39%, 85%)",
+        "light-grayish-blue-hover": "hsl(236, 33%, 92%)",
+        "dark-grayish-blue": "hsl(234, 11%, 52%)",
+        "very-dark-grayish-blue": "hsl(233, 14%, 35%)",
+        "very-dark-grayish-blue-alt": "hsl(237, 14%, 26%)",
+      },
+    },
+    fontFamily: {
+      sans: ["Josefin Sans", "sans-serif"],
+    },
   },
   plugins: [],
 };
