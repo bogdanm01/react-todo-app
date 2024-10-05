342709623523832469195a93a41a6f3db2a91435
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2024-10-05 22:33:55 +0200

    Add todo items list and footer to app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,25 @@ import Header from "./components/header";
 import "./App.css";
 import TodoItemsList from "./components/todo-items-list";
 import NewTodo from "./components/new-todo";
-
-const dummyItems = [
-  {
-    id: 1,
-    taskText: "Finish JavaScript online course",
-    completed: true,
-  },
-  {
-    id: 2,
-    taskText: "Practice German",
-    completed: false,
-  },
-  {
-    id: 3,
-    taskText: "10 minutes meditation",
-    completed: false,
-  },
-  {
-    id: 4,
-    taskText: "Jog around the park 3x",
-    completed: false,
-  },
-];
+import Footer from "./components/footer";
 
 const App = () => {
   const [todoItems, setTodoItems] = useState([]);
+
+  const uncompletedItemsCount = todoItems.filter(
+    (item) => !item.completed
+  ).length;
+
+  const handleChecked = (itemId) => {
+    setTodoItems((items) => {
+      return items.map((item) => {
+        if (item.id === itemId) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      });
+    });
+  };
 
   return (
     <div className="min-h-screen bg-light-very-light-gray dark:bg-dark-very-dark-blue">
@@ -36,7 +29,18 @@ const App = () => {
       <div className="max-w-[32rem] -my-[13.6rem] mx-auto">
         <Header />
         <NewTodo setTodoItems={setTodoItems} />
-        {/* <TodoItemsList /> */}
+
+        {todoItems.length > 0 && (
+          <>
+            <div className="mt-5 drop-shadow-xl bg-dark-very-dark-desaturated-blue rounded">
+              <TodoItemsList items={todoItems} handleChecked={handleChecked} />
+              <Footer uncompletedItemsCount={uncompletedItemsCount} />
+            </div>
+            <p className="text-center mt-10 pb-4 text-[13px] text-dark-very-dark-grayish-blue">
+              Drag and drop to reorder list
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
