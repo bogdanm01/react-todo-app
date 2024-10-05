e096d4ecfa5b2a0901abd9a37a1fccf9fb9a4cfb
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2024-10-05 22:31:45 +0200

    Implement Footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer = ({ uncompletedItemsCount }) => {
+  return (
+    <div className="text-[12px] w-full px-6 py-4 overflow-hidden flex justify-between text-dark-dark-grayish-blue items-end">
+      <p>
+        {uncompletedItemsCount}{" "}
+        {`${uncompletedItemsCount === 1 ? "item" : "items"}`} left
+      </p>
+      <span className="flex gap-3 text-sm pl-4">
+        <p className="cursor-pointer hover:text-bright-blue">All</p>
+        <p className="cursor-pointer hover:text-bright-blue">Active</p>
+        <p className="cursor-pointer hover:text-bright-blue">Completed</p>
+      </span>
+      <p className="cursor-pointer hover:text-bright-blue">Clear completed</p>
+    </div>
+  );
 };
 
 export default Footer;
