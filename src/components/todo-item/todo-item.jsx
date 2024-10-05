ae6a93437b8cb3e5ff1ea9395ceb647300b6b9ee
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2024-10-05 23:11:59 +0200

    Update todo item styling

--- src/components/todo-item/todo-item.jsx
+++ src/components/todo-item/todo-item.jsx
@@ -5,12 +5,12 @@ const TodoItem = ({ item, handleChecked }) => {
   let { completed, id } = item;
 
   return (
-    <div className="flex items-center gap-[1.125rem] px-5 py-[1.05rem] overflow-hidden border-b border-b-dark-very-dark-grayish-blue-alt">
+    <div className="select-none flex items-center gap-[1.125rem] px-5 py-[1.05rem] overflow-hidden border-b border-b-dark-very-dark-grayish-blue-alt">
       <Checkbox checked={completed} handleChecked={handleChecked} itemId={id} />
       <p
         className={`${
           completed && "line-through text-dark-very-dark-grayish-blue"
-        } pt-[0.15rem] text-dark-light-grayish-blue`}
+        } pt-[0.15rem] text-dark-light-grayish-blue select-text`}
       >
         {item.taskText}
       </p>
