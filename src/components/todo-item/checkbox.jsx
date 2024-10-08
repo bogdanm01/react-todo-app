ffc335115c53e0c276bea6f8422a5d1c859fda41
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2024-10-08 19:22:02 +0200

    Update styling

--- src/components/todo-item/checkbox.jsx
+++ src/components/todo-item/checkbox.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 const Checkbox = ({ checked, handleChecked, itemId }) => {
   return (
     <>
       {checked ? (
         <div
           onClick={() => handleChecked(itemId)}
-          className="min-h-[1.4rem] min-w-[1.4rem] flex items-center justify-center rounded-full cursor-pointer bg-check-gradient"
+          className="min-h-[1.4rem] min-w-[1.4rem] flex items-center justify-center rounded-full cursor-pointer bg-check-gradient border-light-very-light-grayish-blue dark:border-dark-very-dark-grayish-blue-alt"
         >
           <img
             className="select-none"
